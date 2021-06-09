376f5cbd2e66e0e79006cc401a5b8eed1a92465d
Yile Ou <troy.yile@gmail.com>
2021-06-10 00:22:50 +0100

    bash script for generating upright variants

---
+++ generateUprightThemes.bash
@@ -0,0 +1,17 @@
+if [ -d themes/upright ]; then
+  rm -r themes/upright
+fi
+cp -R themes/default themes/upright
+
+cd themes/upright
+for file in *.json; do
+  newFile=`echo $file | sed 's/-color-theme.json/-upright-color-theme.json/'`
+  mv $file $newFile
+done
+for file in *.json; do 
+  sed -i '' 's/"fontStyle": "italic",//g' $file;
+  sed -i '' 's/italic underline/underline/g' $file;
+  sed -i '' 's/bold italic/bold/g' $file;
+done
+
+cd ..
